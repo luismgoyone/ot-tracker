bb95e3b0537077b7e2127fa0c9acee1252a00737
Luis Goyone <luismichaelgoyone@gmail.com>
2023-09-20 18:11:52 +0800

    Add GoogleLogin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 import { useState, useEffect } from 'react';
 import './App.css';
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from 'jwt-decode';
 
 function App() {
+  const [credentials, setCredentials] = useState();
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    if (credentials && credentials.credential) {
+      const decodedUser = jwt_decode(credentials.credential);
+      setUser(decodedUser);
+    }
+  }, [credentials]);
 
   console.log(user);
   return (
     <>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          setCredentials(credentialResponse);
+        }}
+        onError={() => {
+          console.log('Login Failed');
+        }}
+      />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GOOGLE_CLIENT_ID } from '../config.json';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
